515c1e3567ec621b5a8782042ed61f1d9c8abe83
Mario Wenzel <maweki@gmail.com>
2017-01-12 16:25:06 +0100

    pocketchip install

---
+++ setup_pocketchip.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install python3 python3-pip python3-dev python python-pip python-dev pv stow htop locales htop
+sudo apt-get remove chip-pico-8 chip-sunvox
+sudo dpkg-reconfigure locales
+sudo apt-get upgrade
+pip3 install --user rtv
+pip3 install --user haxor-news
