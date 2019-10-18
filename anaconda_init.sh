ec509c17da3d6c655e2b49ad63d595efdef43ca7
Mario Wenzel <maweki@gmail.com>
2019-10-18 16:28:56 +0200

    update anaconda installer

--- anaconda_init.sh
+++ anaconda_init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -o /tmp/Anaconda3.sh https://repo.anaconda.com/archive/Anaconda3-5.3.1-Linux-x86_64.sh
+curl -o /tmp/Anaconda3.sh https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
 bash /tmp/Anaconda3.sh -b -p ${HOME}/.local/anaconda3
 rm /tmp/Anaconda3.sh
