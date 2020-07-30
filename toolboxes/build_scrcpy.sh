77eefac3a2765cf9af87b0c520b30e884d820b74
Mario Wenzel <maweki@gmail.com>
2020-07-30 16:27:31 +0200

    scrcpy build script

---
+++ toolboxes/build_scrcpy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#https://github.com/Genymobile/scrcpy/blob/master/BUILD.md
+
+# enable RPM fusion free
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+# client build dependencies
+sudo dnf install -y SDL2-devel ffms2-devel meson gcc make
+
+# server build dependencies
+sudo dnf install -y java-devel
+
+# android tools and graphics driver
+sudo dnf install -y android-tools mesa-dri-drivers
+
+cd /tmp
+git clone https://github.com/Genymobile/scrcpy
+cd scrcpy
+
+source ~/.bashrc
+meson x --buildtype release --strip -Db_lto=true
+ninja -Cx
+sudo ninja -Cx install
