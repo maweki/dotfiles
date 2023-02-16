a2a6970e134388a6492de85aab502196ebbc587b
Mario Wenzel <maweki@gmail.com>
2023-02-16 11:03:07 +0100

    connect scrcpy with bluetooth as well

--- common/run_scrcpy.sh
+++ common/run_scrcpy.sh
@@ -1,6 +1,6 @@
 which scrcpy &> /dev/null || exit 0
 while true
 do
-  sleep 30
-  lsusb -d 18d1:4ee2 && scrcpy -Sw
+  sleep 10
+  lsusb -d 18d1:4ee2 && ( bluetoothctl connect E8:78:29:C2:5B:14 & scrcpy -Sw & wait && bluetoothctl disconnect E8:78:29:C2:5B:14 )
 done
